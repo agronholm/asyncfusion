a776868c82d365e2ae44c6823c959d35aaceab9d
Alex Grönholm <alex.gronholm@nextday.fi>
2024-03-16 13:58:38 +0200

    Fail compilation on any warning

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
+from __future__ import annotations
+
 import platform
 
 from setuptools import Extension, setup
 
 extensions = []
 if platform.system() == "Linux":
-    extensions.append(Extension(
-        "asyncfusion._io_uring",
-        extra_compile_args=["-std=c99"],
-        py_limited_api=True,
-        sources=["src/cextension/io_uring.c"],
-        libraries=['uring'],
-    ))
+    extensions.append(
+        Extension(
+            "asyncfusion._io_uring",
+            extra_compile_args=["-std=c99", "-Werror"],
+            py_limited_api=True,
+            sources=["src/cextension/io_uring.c"],
+            libraries=["uring"],
+        )
+    )
 
 setup(ext_modules=extensions)
